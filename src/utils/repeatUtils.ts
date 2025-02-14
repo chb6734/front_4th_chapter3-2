94a00b79d98c436aafd8300de17d41af8fd67262
chb6734 <chb6734@gmail.com>
2025-02-14 09:01:10 +0900

    feat: 반복 일정 계산하는 utils 추가

--- src/utils/repeatUtils.ts
+++ src/utils/repeatUtils.ts
@@ -1,17 +1,80 @@
 import { EventForm, Event } from '../types';
 
 export function generateRepeatingEvents(event: Event): EventForm[] {
+  const { repeat } = event;
+
+  if (repeat.type === 'none') {
+    return [event];
+  } else if (repeat.type === 'daily') {
+    return getDailyRepeatingEvents(event);
+  } else if (repeat.type === 'weekly') {
+    return getWeeklyRepeatingEvents(event);
+  } else if (repeat.type === 'monthly') {
+    return getMonthlyRepeatingEvents(event);
+  }
+
   return [event];
 }
 
 export function getDailyRepeatingEvents(event: Event): EventForm[] {
-  return [event];
+  const { date, repeat } = event;
+  const { interval, endDate } = repeat;
+
+  const convertedDate = new Date(date);
+
+  const repeatingEvents: EventForm[] = [];
+
+  for (let i = 0; i < interval; i++) {
+    if (convertedDate.toISOString() === endDate) break;
+    const repeatingDate = new Date(date);
+    repeatingEvents.push({
+      ...event,
+      date: repeatingDate.toISOString(),
+    });
+    convertedDate.setDate(convertedDate.getDate() + 1);
+  }
+
+  return repeatingEvents;
 }
 
 export function getWeeklyRepeatingEvents(event: Event): EventForm[] {
-  return [event];
+  const { date, repeat } = event;
+  const { interval, endDate } = repeat;
+
+  const convertedDate = new Date(date);
+
+  const repeatingEvents: EventForm[] = [];
+
+  for (let i = 0; i < interval; i++) {
+    if (convertedDate.toISOString() === endDate) break;
+    const repeatingDate = new Date(convertedDate);
+    repeatingEvents.push({
+      ...event,
+      date: repeatingDate.toISOString(),
+    });
+    convertedDate.setDate(convertedDate.getDate() + 7);
+  }
+
+  return repeatingEvents;
 }
 
 export function getMonthlyRepeatingEvents(event: Event): EventForm[] {
-  return [event];
+  const { date, repeat } = event;
+  const { interval, endDate } = repeat;
+
+  const convertedDate = new Date(date);
+
+  const repeatingEvents: EventForm[] = [];
+
+  for (let i = 0; i < interval; i++) {
+    if (convertedDate.toISOString() === endDate) break;
+    const repeatingDate = new Date(convertedDate);
+    repeatingEvents.push({
+      ...event,
+      date: repeatingDate.toISOString(),
+    });
+    convertedDate.setMonth(convertedDate.getMonth() + 1);
+  }
+
+  return repeatingEvents;
 }
